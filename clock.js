4971021663a0fb879637f4cd696b8d517aa491ce
Arrianne Barker <arrianneoshea@hotmail.co.uk>
2017-11-13 17:03:06 +0000

    changing background colour gradually

--- clock.js
+++ clock.js
@@ -39,12 +39,23 @@ setDate();
 
 //For every second the secondHand moves I would like the background color to change by only one shade.
 
-
-
-
-
-
-
+// document.body.style.backgroundColor = "#AAAAAA";
+//
+//
+// var colors = ["blue", "yellow", "red", "green", "orange"]
+// var currentColor = 0
+// var lis = document.querySelectorAll("body")
+// function changeColor() {
+//   --currentColor
+//   if (currentColor < 0) currentColor = colors.length -1
+//   for (var i = 0; i < lis.length; i++) {
+//     lis[i].style.color = colors[(currentColor +i) % colors.length]
+//   }
+// }
+// setInterval(changeColor, 1000);
+//
+//
+//
 // var div = document.getElementById("html");
 //
 // function getRandomColor() {
