87b497b5f81ae4a508c7eb76bf9050c8563082a2
Arrianne Barker <arrianneoshea@hotmail.co.uk>
2017-11-13 16:08:50 +0000

    called setDate() at the end to make sure clock starts in right place

--- clock.js
+++ clock.js
@@ -3,7 +3,8 @@
 const secondHand = document.querySelector('.second-hand');
 const minsHand = document.querySelector('.min-hand');
 const hourHand = document.querySelector('.hour-hand');
-const allHands = document.querySelectorAll('.hand')
+const allHands = document.querySelectorAll('.hand');
+
 
 
 function setDate() {
@@ -21,6 +22,10 @@ function setDate() {
   const hourDegrees = ((hour / 12) * 360) + ((mins/60)*30) + 90;
   hourHand.style.transform = `rotate(${hourDegrees}deg)`;
 
+
+
+// https://medium.com/@mike_ekkel/javascript-30-day-2-css-js-clock-ae539f10a9f9
+
   if(secondsDegrees === 90) {
       allHands.forEach(hand => hand.style.transition = 'none')
   } else {
@@ -28,4 +33,31 @@ function setDate() {
   }
 }
 
+
 setInterval(setDate, 1000);
+setDate();
+
+//For every second the secondHand moves I would like the background color to change by only one shade.
+
+
+
+
+
+
+
+// var div = document.getElementById("html");
+//
+// function getRandomColor() {
+//     var letters = '0123456789ABCDEF';
+//     var color = '#';
+//     for (var i = 0; i < 6; i++ ) {
+//         color += letters[Math.floor(Math.random() * 16)];
+//     }
+//     return color;
+// }
+//
+// function changeColor(){
+//   div.style.backgroundColor= getRandomColor();
+// }
+//
+// setInterval(changeColor,1000);
