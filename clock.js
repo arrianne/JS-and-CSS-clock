55457abae920df676d8e000a462e9bcb3324b19f
arrianne <arrianneoshea@hotmail.co.uk>
2018-09-14 14:36:43 +0100

    adding new styling to the hill

--- clock.js
+++ clock.js
@@ -9,7 +9,7 @@ const allHands = document.querySelectorAll('.hand');
 
 function setDate() {
   const now = new Date();
-
+console.log(now.getHours() > 18);
   const seconds = now.getSeconds();
   const secondsDegrees = ((seconds / 60) * 360) + 90;
   secondHand.style.transform = `rotate(${secondsDegrees}deg)`;
